c49cfaf27d4f50a9705cb64a2ee7dc2df9e6953e
Dante <frostu8@protonmail.com>
2021-02-09 11:53:00 -0500

    Fix decryption extension maps

--- www/mods/gomori-sdk/scripts/GOMORI SDK.js
+++ www/mods/gomori-sdk/scripts/GOMORI SDK.js
@@ -82,10 +82,10 @@ function decryptAllFiles() {
         switch (ext) {
             case '.KEL':
             case '.AUBREY':
-            case '.PLUTO':
                 return '.json';
             case '.HERO':
-                return '.yml';
+            case '.PLUTO':
+                return '.yaml';
             case '.OMORI':
                 return '.js';
             case '.rpgmvp':
